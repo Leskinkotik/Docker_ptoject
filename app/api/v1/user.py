4b56a70739c5341d92c8b1d08a2968636df358d3
Lesia <leskinkotik@gmail.com>
2024-05-16 14:46:55 +0200

    🐛 the code was added

---
+++ app/api/v1/user.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/user")
+async def read_items():
+    return [{"user_name" : "Adriane"}, {"user_name" : "Lesia"}]
+
+@router.get("/user_all")
+async def get_all():
+    calc = 2 + 9
+    return[{"CouCou" : "Hello World"}, {"All" : "super"}, {"calc":calc}]
